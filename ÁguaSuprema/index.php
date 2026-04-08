eaceb1000a4969b6b92b876cf9e5e90ed7a64ce8
Guilherme Ubiratan <guilherme10ub@gmail.com>
2026-04-08 10:24:23 -0300

    Update index.php

--- ÁguaSuprema/index.php
+++ ÁguaSuprema/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles.css">
-    <title>Loja - Água Suprema</title>
+    <title>Loja virtual - Água Suprema</title>
 </head>
 <body>
     <!--cabeça da página-->
